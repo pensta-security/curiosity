92acc550ab3e31bfa8fd57441faa4bb77f0d8d36
pensta-security <pensta.org@outlook.com>
2025-04-15 13:15:39 +0100

    added api

---
+++ system/curiosity-api/api.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Curiosity API is running"}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
