f5d6201d12700b4722ef5d2aed28813fe01d8ab5
pensta-security <pensta.org@outlook.com>
2025-04-17 14:56:38 +0100

    added update

---
+++ system/curiosity-web/app/routes/system.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, render_template, redirect, url_for, flash, request, jsonify
+
+system = Blueprint('system', __name__)
+
+@system.route('/')
+def index():
+    """Main page"""
+    return render_template('system/index.html')
+
+
+@system.route('/update')
+def update():
+    """update page"""
+    return jsonify({"message": "Manifest is running"}), 200
